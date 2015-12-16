f327344559f44636dc5318c8696b3ac2d545718b
Tom Ashworth <tashworth@twitter.com>
2015-12-16 14:58:04 +0000

    feat: fetch group core
    Implementation of the core of the FetchGroup, composing the plugin
    and filter methods. Tested.
    
    Close #30.

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,11 @@
 /// <reference path="./.d.ts"/>
 "use strict";
-import composeEvery from "./composeEvery";
-import composePromise from "./composePromise";
-import composeVoid from "./composeVoid";
-import getBoundImplementations from "./getBoundImplementations";
 
-export class Request {
-  method: string = "GET";
-  url: string;
-  constructor(input: string | Request, init?: FetchRequestInit)  {
-    if (typeof input === "string") {
-      this.url = input;
-    } else {
-      this.url = input.url;
-    }
-  }
-}
+export class Request implements FetchRequest {}
+export class Response implements FetchResponse {}
 
-export class Response {}
-
-export class FetchGroup implements FetchEnginePlugin, FetchEngineFilter {
-  constructor(opts: FetchGroupOptions = {}) {
-    const { plugins = [] }: FetchGroupOptions = opts;
-
-    this.testRequest =
-      composeEvery(getBoundImplementations("testRequest", plugins));
-    this.shouldFetch =
-      composeEvery(getBoundImplementations("shouldFetch", plugins));
-    this.getRequest =
-      composePromise(getBoundImplementations("getRequest", plugins));
-    this.willFetch =
-      composeVoid(getBoundImplementations("willFetch", plugins));
-    this.fetch =
-      composeVoid(getBoundImplementations("fetch", plugins));
-    this.getRequest =
-      composePromise(getBoundImplementations("getResponse", plugins));
-    this.testResponse =
-      composeEvery(getBoundImplementations("testResponse", plugins));
-    this.didFetch =
-      composeVoid(getBoundImplementations("didFetch", plugins));
-  }
-  testRequest(req: Request): boolean {
-    return true;
-  }
-  shouldFetch(req: Request): boolean {
-    return true;
-  }
-  getRequest(req: Request): Promise<Request> {
-    return Promise.resolve(req);
-  }
-  willFetch(req: Request): void {}
-  fetch(args: FetchArgs): void {}
-  testResponse(req: Response): boolean {
-    return true;
-  }
-  getResponse(res: Response): Promise<Response> {
-    return Promise.resolve(res);
-  }
-  didFetch(res: Response): void {}
-}
-
-export function fetchEngine(group: FetchGroup): Fetch {
-  return function (request: string, init?: FetchRequestInit): Promise<Response> {
+export function fetchEngine(plugin: FetchEnginePlugin): Fetch {
+  return function (request: string): Promise<FetchResponse> {
     return Promise.resolve(new Response());
   };
 };
