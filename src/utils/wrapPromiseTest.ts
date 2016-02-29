cd54bc0df73382fe592fd5f9bad87bdea08d096d
Tom Ashworth <tashworth@twitter.com>
2016-02-29 18:50:30 +0000

    test: basic testem setup using available launchers
    Address #16

--- src/utils/wrapPromiseTest.ts
+++ src/utils/wrapPromiseTest.ts
@@ -1,8 +1,10 @@
 /// <reference path="../.d.test.ts"/>
+import es6Promise = require("es6-promise");
+const Promise = es6Promise.Promise;
 
 export default function wrapPromiseTest(
   test: TapeTest
-): TapeTest  {
+): TapeTest {
   return (t: TapeTestAssertions) =>
     Promise.resolve(test(t))
       .catch(why => t.fail(why));
