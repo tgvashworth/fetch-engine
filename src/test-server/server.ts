7ff0adf061808fd24cc937c43a547428d6a6b0d5
Tom Ashworth <tashworth@twitter.com>
2016-04-04 15:38:09 +0100

    feat: tests running through browserstack

--- src/test-server/server.ts
+++ src/test-server/server.ts
@@ -1,12 +1,9 @@
 /// <reference path="../.d.test.ts" />
 "use strict";
-import yargs = require("yargs");
 import run = require("browser-run");
 
 import Browserstack from "./browserstack";
 import Tunnel from "./tunnel";
-
-const argv = yargs.argv;
 
 function timeout(t: number, v: any): Promise<any> {
   return new Promise(resolve => setTimeout(() => resolve(v), t));
@@ -15,54 +12,65 @@ function timeout(t: number, v: any): Promise<any> {
 // BrowserStack sessions
 
 const browserstack = new Browserstack(
-  argv["browserstack-user"],
-  argv["browserstack-key"]
+  process.env.BROWSERSTACK_USERNAME,
+  process.env.BROWSERSTACK_KEY
 );
 
 // BrowserStack tunnel
 
 const tunnel = new Tunnel({
-  key: argv["browserstack-key"]
+  key: process.env.BROWSERSTACK_KEY
 });
+
+const browsers = [
+  {
+      os: "OS X",
+      os_version: "Lion",
+      browser: "Chrome",
+      browser_version: "47",
+      "browserstack.video": true,
+      "browserstack.local": true
+  },
+  {
+      os: "Windows",
+      os_version: "10",
+      browser: "Chrome",
+      browser_version: "47",
+      "browserstack.video": true,
+      "browserstack.local": true
+  }
+];
+
+function runBrowser(capabilities, id): Promise<void> {
+  return browserstack.createSession(capabilities)
+    .then(session =>
+      session.get(`http://localhost:5000/${id}`)
+        .then(() => session))
+    .then(v => timeout(5000, v))
+    .then(session => session.quit());
+}
 
 tunnel.start()
-  .then(() => {
-    console.log("BrowserStack Tunnel: started");
+  .then(() => Promise.all(browsers.map(runBrowser)))
+  .then(
+    () => exit(0),
+    err => {
+      console.error(err);
+      exit(1);
+    }
+  );
 
-    return browserstack
-      .createSession({
-        os: "OS X",
-        os_version: "Lion",
-        browser: "Chrome",
-        browser_version: "47",
-        "browserstack.video": true,
-        "browserstack.local": true
-      })
-      .then(session =>
-        session.get("http://localhost:5000")
-          .then(() => session))
-      .then(v => timeout(5000, v))
-      .then(session => {
-        console.log("Terminating session");
-        return session.quit();
-      })
-      .then(() => {
-        console.log("done");
-        return tunnel.stop();
-      });
-  })
-  .catch(err => console.error(err));
+process.on("SIGINT", () => exit(1));
 
-process.on("SIGINT", () => {
-  console.log("BrowserStack Tunnel: stopping");
-  tunnel.stop()
-    .then(() => process.exit());
-});
+function exit(code: number = 0): Promise<void> {
+  return tunnel.stop()
+    .then(() => process.exit(code));
+}
 
 // Test code static file host
 
 const browser = run({
-  port: argv["static-port"]
+  port: 5000
 });
 
 process.stdin
