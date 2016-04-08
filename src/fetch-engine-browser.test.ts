55ce5e242684d716b15f55ce4c69cfad3e723dd5
Tom Ashworth <tgvashworth@gmail.com>
2016-04-08 10:51:45 +0100

    test: first test of browser fetch-engine
    Fix #70

---
+++ src/fetch-engine-browser.test.ts
@@ -0,0 +1,27 @@
+/// <reference path="./.d.test.ts" />
+"use strict";
+import test = require("tape");
+import wrap from "./utils/wrapPromiseTest";
+import Request from "./Request";
+import fetchEngine = require("./fetch-engine-browser");
+
+test("browser fetchEngine is requireable", t => {
+  t.plan(1);
+  t.ok(fetchEngine);
+});
+
+test("browser fetchEngine can make a request to the test server", wrap(t => {
+  t.plan(2);
+  const req = new Request(
+    `/echo/text/${encodeURIComponent("Hello fetch-browser")}`
+  );
+  const fetch = fetchEngine();
+  return fetch(req)
+    .then(res => {
+      t.ok(res);
+      return res.text();
+    })
+    .then(text => {
+      t.equal(text, "Hello fetch-browser");
+    });
+}));
