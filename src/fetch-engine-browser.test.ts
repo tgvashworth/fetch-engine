9d1f5d72bfccb369d3e965398362ddc25bec7ac5
Walter Carvalho <waltervascarvalho@gmail.com>
2017-11-14 13:52:48 +0000

    Test fetch-engine-browser for correct API being requirable

--- src/fetch-engine-browser.test.ts
+++ src/fetch-engine-browser.test.ts
@@ -2,12 +2,26 @@
 "use strict";
 import test = require("tape");
 import wrap from "./utils/wrapPromiseTest";
-import Request from "./Request";
-import fetchEngine from "./fetch-engine-browser";
+import fetchEngine, { Request, FetchGroup, Response } from "./fetch-engine-browser";
 
 test("browser fetchEngine is requireable", t => {
   t.plan(1);
   t.ok(fetchEngine);
+});
+
+test("browser FetchGroup is requireable", t => {
+  t.plan(1);
+  t.ok(FetchGroup);
+});
+
+test("browser Request is requireable", t => {
+  t.plan(1);
+  t.ok(Request);
+});
+
+test("browser Response is requireable", t => {
+  t.plan(1);
+  t.ok(Response);
 });
 
 test("browser fetchEngine can make a request to the test server", wrap(t => {
